539c240c25d54c058b6ccc09761a8ff2ed879bee
Roberto Di Lillo <roberto.dilillo@garycom.com>
2024-11-17 08:40:37 +0100

    update main style

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import CameraFeed from '../components/CameraFeed';
-
+import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
@@ -11,10 +11,10 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div>
+      <main className={styles.main}>
         <h1>Emotion Recognition Demo</h1>
         <CameraFeed />
-      </div>
+      </main>
     </>
   );
 }
