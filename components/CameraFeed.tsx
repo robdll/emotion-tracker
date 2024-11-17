a131cf4e55f1f119bd2b32747a257499d02ac6f3
Roberto Di Lillo <roberto.dilillo@garycom.com>
2024-11-17 08:30:30 +0100

    update feed

--- components/CameraFeed.tsx
+++ components/CameraFeed.tsx
@@ -1,7 +1,9 @@
 import { useRef, useEffect } from 'react';
+import * as faceapi from 'face-api.js';
 
 const CameraFeed: React.FC = () => {
   const videoRef = useRef<HTMLVideoElement>(null);
+  const canvasRef = useRef<HTMLCanvasElement>(null);
 
   useEffect(() => {
     const startCamera = async () => {
@@ -15,11 +17,41 @@ const CameraFeed: React.FC = () => {
       }
     };
 
+    const loadModels = async () => {
+      await faceapi.nets.tinyFaceDetector.loadFromUri('/models');
+      await faceapi.nets.faceExpressionNet.loadFromUri('/models');
+    };
+
+    const detectEmotions = async () => {
+      if (!videoRef.current || !canvasRef.current) return;
+
+      const video = videoRef.current;
+      const canvas = canvasRef.current;
+      const displaySize = { width: video.width, height: video.height };
+
+      faceapi.matchDimensions(canvas, displaySize);
+
+      setInterval(async () => {
+        const detections = await faceapi
+          .detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
+          .withFaceExpressions();
+
+        const resizedDetections = faceapi.resizeResults(detections, displaySize);
+        const context = canvas.getContext('2d');
+        if (context) {
+          context.clearRect(0, 0, canvas.width, canvas.height);
+          faceapi.draw.drawDetections(canvas, resizedDetections);
+          faceapi.draw.drawFaceExpressions(canvas, resizedDetections);
+        }
+      }, 100);
+    };
+
     startCamera();
+    loadModels().then(() => detectEmotions());
   }, []);
 
   return (
-    <div>
+    <div style={{ position: 'relative', width: '640px', height: '480px' }}>
       <video
         ref={videoRef}
         autoPlay
@@ -28,6 +60,16 @@ const CameraFeed: React.FC = () => {
         height="480"
         style={{ border: '1px solid #ccc', borderRadius: '8px' }}
       ></video>
+      <canvas
+        ref={canvasRef}
+        width="640"
+        height="480"
+        style={{
+          position: 'absolute',
+          top: 0,
+          left: 0,
+        }}
+      ></canvas>
     </div>
   );
 };
