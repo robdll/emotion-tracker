27126d7d13f91468271c877086309dedf658ee11
Roberto Di Lillo <roberto.dilillo@garycom.com>
2024-11-17 08:50:15 +0100

    btn styles

--- components/CameraFeed.tsx
+++ components/CameraFeed.tsx
@@ -1,5 +1,6 @@
 import { useRef, useEffect, useState } from 'react';
 import * as faceapi from 'face-api.js';
+import styles from '../styles/Home.module.css';
 
 const CameraFeed: React.FC = () => {
   const videoRef = useRef<HTMLVideoElement>(null);
@@ -65,41 +66,31 @@ const CameraFeed: React.FC = () => {
     }, 100);
   };
 
+  const handleBtnClick = () => {
+    if (isCameraOn){
+      stopCamera();
+    } else{
+      startCamera();
+    }
+  }
+
   return (
-    <div style={{ textAlign: 'center' }}>
-      <div style={{ position: 'relative', width: '640px', height: '480px', margin: '0 auto' }}>
+    <>
+      <button onClick={handleBtnClick} className={styles.btn}>
+        {isCameraOn ? 'Stop': 'Start'} Camera
+      </button>
+      <div className={styles.videoWrapper}>
         <video
           ref={videoRef}
           autoPlay
           muted
-          width="640"
-          height="480"
-          style={{ display: isCameraOn ? 'block' : 'none', border: '1px solid #ccc', borderRadius: '8px' }}
+          width="100%"
+          height="100%"
+          style={{ display: isCameraOn ? 'block' : 'none' }}
         ></video>
-        <canvas
-          ref={canvasRef}
-          width="640"
-          height="480"
-          style={{
-            position: 'absolute',
-            top: 0,
-            left: 0,
-            display: isCameraOn ? 'block' : 'none',
-          }}
-        ></canvas>
       </div>
-      <div style={{ marginTop: '20px' }}>
-        {!isCameraOn ? (
-          <button onClick={startCamera} style={{ padding: '10px 20px', fontSize: '16px', cursor: 'pointer' }}>
-            Start Camera
-          </button>
-        ) : (
-          <button onClick={stopCamera} style={{ padding: '10px 20px', fontSize: '16px', cursor: 'pointer' }}>
-            Stop Camera
-          </button>
-        )}
-      </div>
-    </div>
+      
+    </>
   );
 };
 
